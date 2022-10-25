289d228634997278ffe7795d6c0e236965c7cfde
oviozz <42685801+oviozz@users.noreply.github.com>
2022-10-25 15:23:04 -0700

    Update price_analyzer.py

--- price_analyzer.py
+++ price_analyzer.py
@@ -4,10 +4,10 @@ class PriceAnalyzer(price_grabber.PriceGrabber):
 
     item_count = 0
 
-    def price_analyze(self, price, title, url, item_count=None):
+    def price_analyze(self, price, title, url, rating, item_count=None):
 
         if int(float(price.replace('$','').replace(',',''))) <= int(max(self._price_range)):
-            print(f'| Product: {title}\n| Price: {price}\n| Product URL: {url}')
+            print(f'| Product: {title}\n| Price: {price}\n| Product URL: {url}\n| Product Rating: {rating}')
             print(f'{len(url) * "_"}')
             self.item_count += 1
 
