f799b171827e0be90556316928074678ac1f5875
oviozz <42685801+oviozz@users.noreply.github.com>
2022-10-25 14:40:27 -0700

    Update price_looker.py

--- price_looker.py
+++ price_looker.py
@@ -4,3 +4,5 @@ class PriceLooker:
         self._item_name = input('What item are you looking for: ')
         self._price_range = input('Price Range (Ex: $10-$100): ').replace('$','').split('-')
 
+    def item_range(self):
+        return self._price_range
