d87781ff59a728fa94f51c72d146019e0be0153c
oviozz <42685801+oviozz@users.noreply.github.com>
2022-10-25 14:39:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 import price_analyzer
 
-
 def main():
     price_checker = price_analyzer.PriceAnalyzer()
     item_grab = price_checker.load_script()
 
-    print(f'{50*","} Prices Found {50*","}')
+    print(f'{50*"="} Prices Found For Less than ${price_checker.item_range()[0]} {50*"="}\n')
+
 
     for key,value in item_grab.items():
-        price_checker.price_analyze(key,value)
+        price_checker.price_analyze(key, value[0], value[1])
 
-    print(f'\n{50*","} Not in Stock {50*","}')
-
-    for key, value in item_grab.items():
-        price_checker.no_stock_item(key,value)
-
-
+    if price_checker.item_count_return() == 0:
+        print(f'{60 * " "}NO ITEM FOUND')
 
 if __name__ == '__main__':
     main()
-    print(100*',')
+    print(f'\n{133*"="}')
 
